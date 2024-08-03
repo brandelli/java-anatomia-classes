86bd65377e39f2808fa9e96794ab6ceb53e07410
Bruno Dorscheidt Brandelli <bdbrandelli@hotmail.com>
2024-08-03 15:13:21 -0300

    Adicionando funcao basica

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,10 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("testinho");
+        System.out.println(somarInts(1,2));
+    }
+
+    public static int somarInts(int primeiroValor, int segundoValor){
+        return primeiroValor + segundoValor;
     }
 }
